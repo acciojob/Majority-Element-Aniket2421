144b67a9c5a86b84ed7444d8c79a44bd71a1e351
Aniket2421 <115690076+Aniket2421@users.noreply.github.com>
2023-04-01 02:19:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+function majorityElement(nums) {
+    const n = nums.length;
+    const freqMap = new Map();
+    
+    for(let i=0; i<n; i++) {
+        freqMap.set(nums[i], freqMap.get(nums[i]) + 1 || 1);
+        if(freqMap.get(nums[i]) > Math.floor(n/2)) {
+            return nums[i];
+        }
+    }
+}
